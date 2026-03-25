370560184ec4340766ec0ee4e47558b6fc75b718
Joost Florijn <joostflorijn@gmail.com>
2026-03-25 15:03:32 +0100

    refactor: rename CodeRepositoryIdentifierParser to CodeRepositoryReferenceParser and other small changes

--- src/main/java/com/prtracker/application/command/AddCodeRepositoryCommand.java
+++ src/main/java/com/prtracker/application/command/AddCodeRepositoryCommand.java
@@ -1,33 +1,29 @@
 package com.prtracker.application.command;
 
 import com.prtracker.application.command.dto.AddCodeRepositoryDto;
-import com.prtracker.application.parser.CodeRepositoryIdentifierParser;
-import com.prtracker.application.parser.ParsedCodeRepository;
+import com.prtracker.application.parser.CodeRepositoryReferenceParser;
+import com.prtracker.application.parser.ParsedCodeRepositoryReference;
 import com.prtracker.domain.entity.CodeRepository;
 import com.prtracker.domain.service.CodeRepositoryService;
 import com.prtracker.domain.valueobject.CodeRepositoryIdentifier;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class AddCodeRepositoryCommand extends Command<AddCodeRepositoryDto, Void> {
-    private final CodeRepositoryIdentifierParser parser;
+    private final CodeRepositoryReferenceParser parser;
     private final CodeRepositoryService codeRepositoryService;
 
     @Override
     protected Void executeInternal(AddCodeRepositoryDto input) {
-        ParsedCodeRepository parsedCodeRepository = parser.parse(input.name());
-        CodeRepositoryIdentifier identifier = parsedCodeRepository.getIdentifier();
+        ParsedCodeRepositoryReference parsedCodeRepositoryReference = parser.parse(input.repositoryReference());
+        CodeRepositoryIdentifier identifier = parsedCodeRepositoryReference.getIdentifier();
 
-        CodeRepository codeRepository = new CodeRepository(identifier, parsedCodeRepository.owner(),
-                parsedCodeRepository.name(), input.name());
+        CodeRepository codeRepository = new CodeRepository(identifier, parsedCodeRepositoryReference.owner(),
+                parsedCodeRepositoryReference.name());
 
         codeRepositoryService.add(codeRepository);
-
-        log.info("Added repository {}", identifier.value());
 
         return null;
     }
